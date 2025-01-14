a22f46447caa2d514fc70340f7a283529c8ea12d
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-15 00:20:59 +0900

    api를 불러오기위한 파일 생성

---
+++ src/api/useWorldTimeApi.ts
@@ -0,0 +1,21 @@
+type TimeApi = {
+  datetime?: string;
+};
+
+const WorldTimeApi = async (timezone: string): Promise<string> => {
+  try {
+    const response = await fetch(
+      `https://worldtimeapi.org/api/timezone/${timezone}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data: TimeApi = await response.json();
+    return data.datetime || "시간 정보를 불러올 수 없습니다.";
+  } catch (error) {
+    console.error("시간 불러오기 오류:", error);
+    return "시간을 불러오지 못했습니다.";
+  }
+};
+
+export default WorldTimeApi;
